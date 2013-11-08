3e64c30e001ab337d2b2ecbffd1c8531a7e993fe
vikhyath <reddy.vikhyath@gmail.com>
2013-11-07 23:34:05 -0500

    better comments for file I/O and corrected mysql connect instance

--- fourth.py
+++ fourth.py
@@ -10,6 +10,13 @@ for line in txt:
   
 txt.close()
 txt = open(file, 'r+')
+
+# The first argument is a string containing the filename. 
+# The second argument is another string containing a few characters describing the way in which the file will be used. 
+# mode can be 'r' when the file will only be read, 'w' for only writing (an existing file with the same name will be erased), 
+# and 'a' opens the file for appending; any data written to the file is automatically added to the end. 'r+' opens the file for both reading and writing. 
+# The mode argument is optional; 'r' will be assumed if it’s omitted.
+
 print 'contents can also be read as'
 print '%sAND ALSO AS' %txt.readline()
 print txt.read()
--- mysql.py
+++ mysql.py
@@ -1,7 +1,7 @@
 #mysql example with python
 import MySQLdb
 
-db=mysql.connect(host='engsup-web-01.rtp.netapp.com', user='web', passwd='web', db='asup')
+db=MySQLdb.connect(host='engsup-web-01.rtp.netapp.com', user='web', passwd='web', db='asup')
 c = db.cursor()
 c.execute("select * from delta_states where id = %d" %(1))
 c.fetchone()
